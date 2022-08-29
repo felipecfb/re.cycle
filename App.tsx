bc06244bb9146986c0bfd299e1c224f3ccfeef0f
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-08-29 10:31:07 -0300

    ✨ feat: Creating a customize theme to native base

--- App.tsx
+++ App.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { NativeBaseProvider } from "native-base";
 
 import { Home } from "./src/screens/Home";
+import { theme } from "./src/styles/theme";
 
 export default function App() {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={theme}>
       <Home />
     </NativeBaseProvider>
   );
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from "native-base";
+
+export const theme = extendTheme({
+  colors: {
+    green: {
+      900: '#37563D',
+    },
+    lime: {
+      200: "#A9B685"
+    },
+    amber: {
+      50: "#FFF9E8",
+    }
+  },
+});
